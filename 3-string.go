c93034e0363db873616414150a8bc0efc7e8dd48
akhmadfaizabdulloh <akhmadfaizabdulloh@gmail.com>
2023-08-30 18:43:07 +0700

    func len() and string[0]

--- 3-string.go
+++ 3-string.go
@@ -6,4 +6,8 @@ func main()  {
 	fmt.Println("Faiz")
 	fmt.Println("Akhmad Faiz")
 	fmt.Println("Akhmad Faiz Abdulloh")
+
+	fmt.Println(len("Faiz"))
+	fmt.Println("Akhmad Faiz"[0])
+	fmt.Println("Akhmad Faiz Abdulloh"[1])
 }
