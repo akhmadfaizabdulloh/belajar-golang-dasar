16c7a15da494919b970ecff5d5ea8b574a880e8f
akhmadfaizabdulloh <akhmadfaizabdulloh@gmail.com>
2023-09-03 17:43:16 +0700

    6-constant.go

---
+++ 6-constant.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func main() {
+	const firstName string = "Akhmad Faiz"
+	const lastName = "Abdulloh"
+	const value = 1000
+
+	// nilai const tidak dapat diubah
+	// firstName = "Arios"
+
+	// nilai const tidak akan di komplain oleh si golang meskipun tidak di gunakan, berbeda dengan variable
+
+	// fmt.Println(firstName)
+	fmt.Println(lastName)
+	fmt.Println(value)
+
+	// deklarasi multiple constant
+	const (
+		namaDepan    = "Akhmad"
+		namaBelakang = "Faiz"
+	)
+
+	fmt.Println(namaDepan)
+	fmt.Println(namaBelakang)
+}
